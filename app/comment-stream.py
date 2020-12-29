68d328d5e68859cf9fdd009da0896d7846643b02
ottomattas <otto.mattas@gmail.com>
2020-12-29 16:00:02 +0100

    Improve formatting

--- app/comment-stream.py
+++ app/comment-stream.py
@@ -27,15 +27,19 @@ from better_profanity import profanity
 
 def remove_emoji(comment):
     emoji_pattern = re.compile("["
-       u"\U0001F600-\U0001F64F"  # emoticons
-       u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-       u"\U0001F680-\U0001F6FF"  # transport & map symbols
-       u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-       u"\U00002702-\U00002f7B0"
-       u"\U000024C2-\U0001F251"
-       "]+", flags=re.UNICODE)
+                               # emoticons
+                               u"\U0001F600-\U0001F64F"
+                               # symbols & pictographs
+                               u"\U0001F300-\U0001F5FF"
+                               # transport & map symbols
+                               u"\U0001F680-\U0001F6FF"
+                               # flags (iOS)
+                               u"\U0001F1E0-\U0001F1FF"
+                               u"\U00002702-\U00002f7B0"
+                               u"\U000024C2-\U0001F251"
+                               "]+", flags=re.UNICODE)
 
-    cleaned_comment =  emoji_pattern.sub(r'', comment)
+    cleaned_comment = emoji_pattern.sub(r'', comment)
 
     return cleaned_comment
 
@@ -50,7 +54,9 @@ def process_or_store(comment):
         response = firehose_client.put_record(
             DeliveryStreamName='<insert-delivery-stream-name>',
             Record={
-                'Data': (json.dumps(comment, ensure_ascii=False) + '\n').encode('utf8')
+                'Data': (json.dumps(
+                                    comment, ensure_ascii=False
+                                    ) + '\n').encode('utf8')
                     }
             )
         logging.info(response)
@@ -60,7 +66,7 @@ def process_or_store(comment):
 
 firehose_client = boto3.client('firehose', region_name="us-east-1")
 LOG_FILENAME = '/tmp/reddit-stream.log'
-logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
+logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
 
 if len(sys.argv) >= 2:
     try:
@@ -80,23 +86,24 @@ if len(sys.argv) >= 2:
             else:
                 is_censored = 0
 
-            cleaned_comment = profanity.censor((remove_emoji(str(comment.body))))
-            comment_date = str(datetime.datetime.utcfromtimestamp(comment.created_utc).strftime('%Y/%m/%d %H:%M:%S'))
+            cleaned_comment = profanity.censor(
+                (remove_emoji(str(comment.body))))
+            comment_date = str(datetime.datetime.utcfromtimestamp(
+                comment.created_utc).strftime('%Y/%m/%d %H:%M:%S'))
             sentiment = get_comment_sentiment(cleaned_comment)
-            pattern_polarity = round(sentiment.polarity,4)
-
+            pattern_polarity = round(sentiment.polarity, 4)
 
             commentjson = {
-            			  '@timestamp' : comment_date,
-                          'comment_id': comment.id,
-                           'subreddit': str(comment.subreddit),
-                           'comment_body': cleaned_comment,
-                           'comment_distinguished': comment.distinguished,
-                           'comment_is_submitter': comment.is_submitter,
-                           'comment_tb_sentiment': pattern_polarity,
-                           'comment_is_censored': is_censored,
-		           'author_name': str(comment.author.name)
-                           }
+                '@timestamp': comment_date,
+                'comment_id': comment.id,
+                'subreddit': str(comment.subreddit),
+                'comment_body': cleaned_comment,
+                'comment_distinguished': comment.distinguished,
+                'comment_is_submitter': comment.is_submitter,
+                'comment_tb_sentiment': pattern_polarity,
+                'comment_is_censored': is_censored,
+                'author_name': str(comment.author.name)
+            }
             print("==================================")
             num_comments_collected = num_comments_collected + 1
             print(num_comments_collected)
@@ -105,4 +112,4 @@ if len(sys.argv) >= 2:
     except Exception as e:
         print(e)
 else:
-    print("please enter subreddit.")
+    print("Please enter subreddit.")
