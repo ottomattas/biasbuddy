190538eb0e5cccd3966e2bb11a1c6f21d764ea9d
ottomattas <otto.mattas@gmail.com>
2020-12-31 12:12:31 +0100

    Initiliase Filippo's training scripts

---
+++ app/training/BiasCalculator.py
@@ -0,0 +1,192 @@
+import time
+
+import gensim
+import nltk
+import numpy as np
+import pandas as pd
+from gensim.models import Word2Vec
+from gensim.models.callbacks import CallbackAny2Vec
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from progress.bar import Bar
+from scipy import spatial
+
+
+class EpochLogger(CallbackAny2Vec):
+    def __init__(self):
+        self.starttime = 0.0
+        self.epoch = 0
+        self.tot_epochs = 100
+        self.single_epoch_time = 0
+
+    def on_epoch_begin(self, model):
+        print(f"Started epoch {self.epoch}.")
+
+    def on_epoch_end(self, model):
+        if self.epoch == 0:
+            self.starttime = time.time()
+            self.single_epoch_time = time.time()
+        else:
+            if self.epoch != self.tot_epochs:
+                print(f"Finished epoch {self.epoch - 1} in {time.time() - self.single_epoch_time}")
+                self.single_epoch_time = time.time()
+            else:
+                print(f"Training finished in {time.time() - self.starttime}s")
+        self.epoch += 1
+
+
+class BiasModel:
+    nltk.download('averaged_perceptron_tagger')
+    nltk.download('vader_lexicon')
+
+    def __init__(self, comments_document, comment_column='body', output_name='outputModel',
+                 window=4, min_frequency=10, out_dimension=200):
+        self.comments_document = comments_document
+        self.comment_column = comment_column
+        self.output_name = output_name
+        self.window = window
+        self.min_frequency = min_frequency
+        self.out_dimension = out_dimension
+        self.sentiment_analyzer = SentimentIntensityAnalyzer()
+
+    @staticmethod
+    def calculate_centroid(model, words):
+        embeddings = [np.array(model[w]) for w in words if w in model]
+        centroid = np.zeros(len(embeddings[0]))
+        for e in embeddings:
+            centroid += e
+        return centroid / len(embeddings)
+
+    @staticmethod
+    def get_cosine_distance(embedding1, embedding2):
+        return spatial.distance.cosine(embedding1, embedding2)
+
+    def load_json_and_preprocess(self, nrowss=None, lemmatise=False):
+        """
+        input:
+        nrowss <int> : number of rows to process, leave None if all
+        tolower <True/False> : transform all text to lowercase
+        returns:
+        List of preprocessed sentences, i.e. the input to train
+        """
+        print(f"Processing document: {self.comments_document}")
+
+        trpCom = pd.read_json(self.comments_document)
+        trpCom.fillna(0)
+        documents = []
+        for i, row in enumerate(trpCom[self.comment_column]):
+            if i % 100000 == 0:
+                print(f'Processing line {i}')
+            try:
+                pp = gensim.utils.simple_preprocess(row)
+                if lemmatise:
+                    pp = [wordnet_lemmatizer.lemmatize(w, pos="n") for w in pp]
+                documents.append(pp)
+            except TypeError:
+                print(f'Row {i} threw a type error.')
+
+        print('Done reading all documents')
+        return documents
+
+    def load_csv_and_preprocess(self, nrowss=None, lemmatise=False):
+        """
+        input:
+        nrowss <int> : number of rows to process, leave None if all
+        tolower <True/False> : transform all text to lowercase
+        returns:
+        List of preprocessed sentences, i.e. the input to train
+        """
+        print(f"Processing document: {self.comments_document}")
+        trpCom = pd.read_csv(self.comments_document, lineterminator='\n', nrows=nrowss)
+        trpCom.fillna(0)
+        documents = []
+        for i, row in enumerate(trpCom[self.comment_column]):
+
+            if i % 100000 == 0:
+                print(f'Processing line {i}')
+            try:
+                pp = gensim.utils.simple_preprocess(row)
+                if lemmatise:
+                    pp = [wordnet_lemmatizer.lemmatize(w, pos="n") for w in pp]
+                documents.append(pp)
+            except TypeError:
+                print(f'Row {i} threw a type error.')
+
+        print('Done reading all documents')
+        return documents
+
+    def train(self, documents, epochs):
+        """
+        documents list<str> : List of texts preprocessed
+        outputfile <str> : final file will be saved in this path
+        ndim <int> : embedding dimensions
+        window <int> : window when training the model
+        minfreq <int> : minimum frequency, words with less freq will be discarded
+        epochss <int> : training epochs
+        """
+
+        epoch_logger = EpochLogger()
+        print(f'Started training model {self.output_name}:\n\tDimensions:{self.out_dimension}, '
+              f'\n\tMinimum word frequency:{self.min_frequency} \n\tEpochs:{epochs}')
+        model = gensim.models.Word2Vec(documents, size=self.out_dimension, window=self.window,
+                                       min_count=self.min_frequency,
+                                       workers=5, callbacks=[epoch_logger])
+        model.train(documents, total_examples=len(documents), epochs=epochs)
+        model.save(self.output_name)
+        print(f'\nModel saved in {self.output_name}')
+
+    def get_top_most_biased_words(self, topk, c1, c2, pos=None):
+        """
+        topk <int> : topk words
+        c1 list<str> : list of words for target set 1
+        c2 list<str> : list of words for target set 2
+        pos list<str> : List of parts of speech we are interested in analysing
+        verbose <bool> : True/False
+        """
+
+        if pos is None:
+            pos = ['JJ', 'JJR', 'JJS']
+
+        model = Word2Vec.load(self.output_name)
+        words_sorted = sorted([(k, v.index, v.count) for (k, v) in model.wv.vocab.items()], key=lambda x: x[1],
+                              reverse=False)
+        words = [w for w in words_sorted if nltk.pos_tag([w[0]])[0][1] in pos]
+
+        try:
+            assert (len(c1) < 1 or len(c2) < 1 or len(words) < 1)
+        except AssertionError:
+            print('[!] Not enough word concepts to perform the experiment')
+
+        centroid1, centroid2 = self.calculate_centroid(model, c1), self.calculate_centroid(model, c2)
+        winfo = []
+
+        print(f"Extracting the {topk} most biased words.")
+        bar = Bar('Training', max=len(words))
+
+        for i, w in enumerate(words):
+            word = w[0]
+            freq = w[2]
+            rank = w[1]
+
+            pos = nltk.pos_tag([word])[0][1]
+
+            wv = model[word]
+
+            sent = self.sentiment_analyzer.polarity_scores(word)['compound']
+
+            d1 = self.get_cosine_distance(centroid1, wv)
+            d2 = self.get_cosine_distance(centroid2, wv)
+
+            bias = d2 - d1
+
+            winfo.append({'word': word, 'bias': bias, 'freq': freq, 'pos': pos, 'wv': wv, 'rank': rank, 'sent': sent})
+            bar.next()
+
+        bar.finish()
+
+        biasc1 = sorted(winfo, key=lambda x: x['bias'], reverse=True)[:min(len(winfo), topk)]
+        biasc2 = sorted(winfo, key=lambda x: x['bias'], reverse=False)[:min(len(winfo), topk)]
+
+        for w2 in biasc2:
+            w2['bias'] = w2['bias'] * -1
+
+        return [biasc1, biasc2]
---
+++ app/training/Run.py
@@ -0,0 +1,187 @@
+# %%
+from collections import defaultdict
+
+import numpy as np
+import pandas as pd
+
+from BiasCalculator import BiasModel
+
+# %%
+# csvpath = 'Datasets/comments_small.csv'
+# outputpath = 'Models/million_the_donald'
+
+# For debug
+csvpath = '"D:\\Downloads\\politics.corpus\\utterances_toy.jsonl"'
+outputpath = 'Models\\odahnfv'
+
+# %%
+'''
+Train new model
+'''
+# print('Training new model', csvpath)
+# DADDBias_ICWSM.TrainModel(csvpath, outputname=outputpath, epochs=5)
+# print('Training finished, saved ', outputpath)
+model = BiasModel(csvpath, output_name=outputpath)
+doc = model.load_json_and_preprocess()
+model.train(doc, 5)
+
+# %%
+'''
+Find biased words
+'''
+print()
+mexican = ["mexican", "mexico", "hispanic", "immigrant", "brown"]
+american = ["american", "america", "white", "caucasian", "yankee"]
+
+women = ["sister", "female", "woman", "girl", "daughter", "she", "hers", "her"]
+men = ["brother", "male", "man", "boy", "son", "he", "his", "him"]
+
+#
+# mex_bias = ['mexican', 'hispanic', 'unverified', 'colombian', 'unauthorized', 'undeported', 'brazilian', 'pedestrian',
+#             'suspicious', 'ianal', 'blasphemous', 'reputable', 'nearest', 'irritated', 'hillarious', 'former', 'upped',
+#             'credible', 'spanish', 'uncounted', 'uninsured', 'latest', 'verifiable', 'mythical', 'notable', 'illegal',
+#             'valid', 'vatican', 'preliminary', 'unsolicited', 'indian', 'porous', 'duncan', 'hawaiian', 'geographical',
+#             'undocumented', 'risky', 'unsecured', 'inadmissible', 'questionable', 'irish', 'unsourced', 'electric',
+#             'provisional', 'unhelpful', 'gross', 'previous', 'explosive', 'atlantic', 'punitive', 'untaxed', 'somalian',
+#             'thelocal', 'green', 'sic', 'unconfirmed', 'humorous', 'utm_term', 'unaccompanied', 'scottish', 'statistic',
+#             'unwanted', 'excessive', 'neverhillary', 'undelete', 'other', 'rican', 'unethical', 'noteworthy', 'trivial',
+#             'adderal', 'chic', 'legal', 'unvetted', 'serial', 'unprotected', 'egregious', 'seasonal', 'invalid',
+#             'crappy', 'unfriended', 'infamous', 'african', 'heinous', 'ridiculous', 'addictive', 'familiar',
+#             'inconclusive', 'permissible', 'sarkeesian', 'probable', 'residential', 'improbable', 'iranian',
+#             'particular', 'scan', 'upkeep', 'occasional', 'nigerian', 'whichhillary', 'negative', 'speculative',
+#             'insured', 'freudian', 'improvised', 'newest', 'unregistered', 'isolated', 'hot', 'southern', 'unreported',
+#             'unsubstantiated', 'controversial', 'injured', 'uptown', 'appalachian', 'topical', 'bannable', 'cumulative',
+#             'taiwanese', 'chronological', 'unlawful', 'disabled', 'unlikely', 'senecal', 'draconian', 'impractical',
+#             'portuguese', 'lithuanian', 'unnamed', 'undisclosed', 'unsubbed', 'solitary', 'unreasonable', 'prospective',
+#             'outrageous', 'impenetrable', 'traumatic', 'nimble', 'ancestral', 'youngest', 'nicaraguan', 'syrian',
+#             'apprehensive', 'gooble', 'statistical', 'seismic', 'replaceable', 'unenforceable', 'unprofessional']
+# amer_bias = ['american', 'western', 'capitalistic', 'utopian', 'classical', 'unified', 'white', 'rigid', 'righteous',
+#              'neoliberal', 'healthy', 'ideological', 'vested', 'communistic', 'contemporary', 'intellectual',
+#              'antagonistic', 'parliamentary', 'unashamed', 'modern', 'authoritarian', 'intersectional',
+#              'neoconservative', 'eternal', 'courageous', 'multicultural', 'global', 'ultimate', 'ultra', 'intrinsic',
+#              'memetic', 'alive', 'unearned', 'manifest', 'social', 'infectious', 'achievable', 'galactic',
+#              'revolutionary', 'unite', 'perpetual', 'undisputed', 'dogmatic', 'traditional', 'unconstrained',
+#              'dystopian', 'totalitarian', 'competitive', 'unseen', 'unapologetic', 'unproductive', 'cultural',
+#              'unquestionable', 'civil', 'greater', 'prosperous', 'conventional', 'unabashed', 'democratic', 'leftish',
+#              'bubble', 'theocratic', 'equal', 'oppressive', 'unicorn', 'corporate', 'assemble', 'cyclical', 'real',
+#              'heretic', 'technological', 'transitional', 'free', 'productive', 'foundational', 'patriotic',
+#              'tyrannical', 'geopolitical', 'pragmatic', 'autocratic', 'inclusive', 'rich', 'general', 'unelected',
+#              'flourish', 'fascistic', 'moral', 'intact', 'unwashed', 'spiritual', 'foreseeable', 'wildest',
+#              'sanctimonious', 'affordable', 'murican', 'unfathomable', 'pyrrhic', 'rebellious', 'economic', 'radical',
+#              'entrepreneurial', 'fashionable', 'victorious', 'treasonous', 'altruistic', 'stable', 'powerful', 'noble',
+#              'bearable', 'uncucked', 'predictable', 'unhindered', 'interested', 'educational', 'academic',
+#              'egalitarian', 'secondary', 'illiberal', 'poisonous', 'individual', 'agrarian', 'industrial', 'tolerable',
+#              'evolutionary', 'magnanimous', 'orwellian', 'opportunistic', 'boisterous', 'instrumental', 'ambitious',
+#              'endemic', 'voluntary', 'united', 'finest', 'basic', 'preferable', 'dynamic', 'great', 'semetic',
+#              'fittest', 'visionary', 'national', 'fiscal', 'superficial', 'inflexible', 'special', 'clear',
+#              'intangible', 'conscious', 'unbearable']
+
+[women_bias, men_bias] = model.get_top_most_biased_words(150,  # topk biased words
+                                                         women,
+                                                         men,
+                                                         ['JJ', 'JJR', 'JJS'])
+
+# [mex_bias, amer_bias] = DADDBias_ICWSM.GetTopMostBiasedWords(outputpath,  # model path
+#                                                              150,  # topk biased words
+#                                                              mexican,  # target set 1
+#                                                              american,  # target set 2
+#                                                              ['JJ', 'JJR', 'JJS'],  # nltk pos to be considered
+#                                                              verbose=False)
+
+print('Biased words towards ', mexican)
+print([b['word'] for b in women_bias])
+print(f'Aggregated sentiment for top 150 words:{np.sum([b["sent"] for b in women_bias])}')
+
+print('Biased words towards ', american)
+print([b['word'] for b in men_bias])
+print(f'Aggregated sentiment for top 150 words:{np.sum([b["sent"] for b in men_bias])}')
+
+# %%
+
+# bias_dict = {'mexican': 0.0, 'hispanic': 0.0, 'unverified': 0.0, 'colombian': 0.0, 'unauthorized': 0.0,
+#              'undeported': 0.0, 'brazilian': 0.0, 'pedestrian': 0.0, 'suspicious': -0.3612, 'ianal': 0.0,
+#              'blasphemous': 0.0, 'reputable': 0.0, 'nearest': 0.0, 'irritated': -0.4588, 'hillarious': 0.0,
+#              'former': 0.0, 'upped': 0.0, 'credible': 0.0, 'spanish': 0.0, 'uncounted': 0.0, 'uninsured': 0.0,
+#              'latest': 0.0, 'verifiable': 0.0, 'mythical': 0.0, 'notable': 0.0, 'illegal': -0.5574, 'valid': 0.0,
+#              'vatican': 0.0, 'preliminary': 0.0, 'unsolicited': 0.0, 'indian': 0.0, 'porous': 0.0, 'duncan': 0.0,
+#              'hawaiian': 0.0, 'geographical': 0.0, 'undocumented': 0.0, 'risky': -0.2023, 'unsecured': -0.3818,
+#              'inadmissible': 0.0, 'questionable': -0.296, 'irish': 0.0, 'unsourced': 0.0, 'electric': 0.0,
+#              'provisional': 0.0, 'unhelpful': 0.0, 'gross': -0.4767, 'previous': 0.0, 'explosive': 0.0, 'atlantic': 0.0,
+#              'punitive': -0.5106, 'untaxed': 0.0, 'somalian': 0.0, 'thelocal': 0.0, 'green': 0.0, 'sic': 0.0,
+#              'unconfirmed': -0.128, 'humorous': 0.3818, 'utm_term': 0.0, 'unaccompanied': 0.0, 'scottish': 0.0,
+#              'statistic': 0.0, 'unwanted': -0.2263, 'excessive': 0.0, 'neverhillary': 0.0, 'undelete': 0.0,
+#              'other': 0.0, 'rican': 0.0, 'unethical': -0.5106, 'noteworthy': 0.0, 'trivial': -0.0258, 'adderal': 0.0,
+#              'chic': 0.2732, 'legal': 0.128, 'unvetted': 0.0, 'serial': 0.0, 'unprotected': -0.3612, 'egregious': 0.0,
+#              'seasonal': 0.0, 'invalid': 0.0, 'crappy': -0.5574, 'unfriended': 0.0, 'infamous': 0.0, 'african': 0.0,
+#              'heinous': 0.0, 'ridiculous': -0.3612, 'addictive': 0.0, 'familiar': 0.0, 'inconclusive': 0.0,
+#              'permissible': 0.0, 'sarkeesian': 0.0, 'probable': 0.0, 'residential': 0.0, 'improbable': 0.0,
+#              'iranian': 0.0, 'particular': 0.0, 'scan': 0.0, 'upkeep': 0.0, 'occasional': 0.0, 'nigerian': 0.0,
+#              'whichhillary': 0.0, 'negative': -0.5719, 'speculative': 0.1027, 'insured': 0.0, 'freudian': 0.0,
+#              'improvised': 0.0, 'newest': 0.0, 'unregistered': 0.0, 'isolated': -0.3182, 'hot': 0.0, 'southern': 0.0,
+#              'unreported': 0.0, 'unsubstantiated': 0.0, 'controversial': -0.2023, 'injured': -0.4019, 'uptown': 0.0,
+#              'appalachian': 0.0, 'topical': 0.0, 'bannable': 0.0, 'cumulative': 0.0, 'taiwanese': 0.0,
+#              'chronological': 0.0, 'unlawful': 0.0, 'disabled': 0.0, 'unlikely': 0.0, 'senecal': 0.0, 'draconian': 0.0,
+#              'impractical': 0.0, 'portuguese': 0.0, 'lithuanian': 0.0, 'unnamed': 0.0, 'undisclosed': 0.0,
+#              'unsubbed': 0.0, 'solitary': 0.0, 'unreasonable': 0.0, 'prospective': 0.0, 'outrageous': -0.4588,
+#              'impenetrable': 0.0, 'traumatic': -0.5719, 'nimble': 0.0, 'ancestral': 0.0, 'youngest': 0.0,
+#              'nicaraguan': 0.0, 'syrian': 0.0, 'apprehensive': 0.0, 'gooble': 0.0, 'statistical': 0.0, 'seismic': 0.0,
+#              'replaceable': 0.0, 'unenforceable': 0.0, 'unprofessional': -0.5106}
+
+bias_dict = dict(zip([b['word'] for b in women_bias], [b["sent"] for b in women_bias]))
+neg_bias_dict = {key: value for (key, value) in bias_dict.items() if np.sign(value) == -1}
+pos_bias_dict = {key: value for (key, value) in bias_dict.items() if np.sign(value) == 1}
+print(f"There are {len(neg_bias_dict)} terms negatively biased towards women.")
+
+# %%
+df = pd.read_csv(csvpath)
+df = df[['created', 'body']]
+df.drop(df[df['body'] == '[deleted]'].index, inplace=True)
+df.reset_index(inplace=True)
+df.sort_values(by=['created'], inplace=True)
+
+df['created'] = pd.to_datetime(df['created'], unit='s')
+
+n_comments_per_day = 0
+daily_neg_bias_dict = defaultdict(int)
+daily_pos_bias_dict = defaultdict(int)
+total_neg_biases_dict = {}
+total_pos_biases_dict = {}
+current_date = df['created'][0].strftime("%d/%m/%Y")
+
+for idx, row in df.iterrows():
+    if current_date != row['created'].strftime("%d/%m/%Y"):
+        total_neg_biases_dict[current_date] = {k: v / n_comments_per_day for (k, v) in daily_neg_bias_dict.items()}
+        total_pos_biases_dict[current_date] = {k: v / n_comments_per_day for (k, v) in daily_pos_bias_dict.items()}
+        # print("#" * 20 + str({idx}) + "#" * 20,
+        #       f"\nDay {current_date} had these biases per word:\n "
+        #       f"{[('Word: ' + w, 'Value: ' + str(f)) for (w, f) in daily_neg_bias_dict.items()]}"
+        #       f"\n out of {n_comments_per_day} comments.")
+        daily_neg_bias_dict = defaultdict(int)
+        daily_pos_bias_dict = defaultdict(int)
+        current_date = row['created'].strftime("%d/%m/%Y")
+        n_comments_per_day = 0
+
+    comment = row['body']
+    for word in str(comment).split():
+        if word in neg_bias_dict.keys():
+            daily_neg_bias_dict[word] += neg_bias_dict[word]
+        elif word in pos_bias_dict.keys():
+            daily_pos_bias_dict[word] += pos_bias_dict[word]
+
+    n_comments_per_day += 1
+
+print("Writing to file...")
+
+transposed_df = pd.DataFrame.from_dict(total_neg_biases_dict).fillna(0).T
+half_elements = int(len(transposed_df.columns) / 2) if len(transposed_df.columns) >= 10 else len(transposed_df.columns)
+drop_columns = transposed_df.sum()[half_elements:]
+drop_columns_lbl = [lbl for lbl in drop_columns.index]
+transposed_df.drop(labels=drop_columns_lbl, axis=1, inplace=True)
+transposed_df.to_csv("Datasets/daily_important_sex_neg.csv")
+
+transposed_df = pd.DataFrame.from_dict(total_pos_biases_dict).fillna(0).T
+half_elements = int(len(transposed_df.columns) / 2) if len(transposed_df.columns) >= 10 else len(transposed_df.columns)
+drop_columns = transposed_df.sum()[half_elements:]
+drop_columns_lbl = [lbl for lbl in drop_columns.index]
+transposed_df.drop(labels=drop_columns_lbl, axis=1, inplace=True)
+transposed_df.to_csv("Datasets/daily_important_sex_pos.csv")
